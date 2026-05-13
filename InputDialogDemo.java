961ffc1004a0d8613112c1d2d4988f8b7e5736a2
Thịnh <thinhhuynh21107@gmail.com>
2026-05-13 09:50:43 +0700

    Delete InputDialogDemo.java
    Signed-off-by: Thịnh <thinhhuynh21107@gmail.com>

--- InputDialogDemo.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class InputDialogDemo {
-    public static void main(String[] args) {
-        String name = JOptionPane.showInputDialog(null, "What is your name?");
-        JOptionPane.showMessageDialog(null, "Welcome, " + name + ", to Java Programming!");
-    }
-}
